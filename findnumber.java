02429ca00918db2b2b59fcfaeb38a7676c629d37
Nitin <nitinprajapati415@gmail.com>
2024-01-01 10:57:25 +0530

    this is first commit

---
+++ findnumber.java
@@ -0,0 +1,5 @@
+public class findnumber{
+    public static void main(String[] args) {
+        System.out.println("nitin");
+    }
+}
---
+++ javawebapp.java
@@ -0,0 +1,5 @@
+public class javawebapp{
+    public static void main(String[] args) {
+        System.out.println("hello guys");
+    }
+}
