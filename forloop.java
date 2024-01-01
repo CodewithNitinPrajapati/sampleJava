34533dd6125fcc711897ae04647b1e5d2d55f6d3
Nitin <nitinprajapati415@gmail.com>
2024-01-01 12:06:51 +0530

    this is fourth commit

--- forloop.java
+++ forloop.java
@@ -2,5 +2,6 @@ public class forloop{
     public static void main(String[] args) {
         System.out.println("this is for loop file ");
         System.out.println("nitin");
+        System.out.println("nitinimdsjsf");
     }
 }
